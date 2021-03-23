ce83063aba27173e79a45b9bcd63cb1d565a227c
Jalpesh Vadgama <Jalpesh@dotnetjalps.com>
2021-03-23 23:23:43 +0530

    change the character

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-const routerOPtions: ExtraOptions = {
+const routerOptions: ExtraOptions = {
   scrollPositionRestoration: 'enabled',
   anchorScrolling: 'enabled',
 };
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, routerOPtions)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
